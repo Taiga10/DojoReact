5a426a4222155ead22d14f344414dbb2769c8be8
Fernanda Vega <fernanda.fujosh1@gmail.com>
2019-07-27 11:33:57 -0500

    dojo react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import Poke from './Pokedex/Poke';
+
+function App() {
+  return (
+    <div><Poke id="25"  /></div>
+  );
+}
+
+export default App;
